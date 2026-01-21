54bdbc4108f8bc5d1b1b190b4601bb880cf668a8
MedSveta <sveta1978medved@gmail.com>
2026-01-21 19:30:44 +0200

    Webinar 1

--- src/main/java/webinar01/DataType.java
+++ src/main/java/webinar01/DataType.java
@@ -1,4 +1,49 @@
 package webinar01;
 
 public class DataType {
+    static int b;
+    static double b1;
+    static char b2;
+    static boolean b3;
+
+    public static void main(String[] args) {
+        // примитивы
+        // целочисленные byte/short/int/long
+        // с плавающей точкой double/float
+        // символьный char
+        // логические boolean
+
+        int a = 0;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+
+        long l = 10_000_000_000L;
+        float f = 6.000008976060f;
+
+        char char1 = 65;
+        System.out.println(char1);
+        char char2 = 'A';
+        System.out.println(char2);
+        System.out.println((int) char2);
+
+        String str1 = "qwerty";
+        String str2 = "qwerty";
+        String str3 = new String("qwerty");
+        if (str1 == str2)
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+        if (str1.equals(str3) == true)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+
+    private void first(){
+
+    }
 }
--- src/main/java/webinar01/Wrappers.java
+++ src/main/java/webinar01/Wrappers.java
@@ -1,4 +1,24 @@
 package webinar01;
 
 public class Wrappers {
+    // int --> Integer
+    // double --> Double
+    // char --> Character
+    // boolean --> Boolean
+    public static void main(String[] args) {
+        int a = 13;
+        Integer b = 45;
+        System.out.println(a + b);
+        String bStr = b.toString();
+        System.out.println(bStr + a);
+        int c = Integer.parseInt("45");
+        System.out.println(a + c);
+        //int v = Integer.parseInt("354df54");
+        System.out.println("================");
+        String[] str = new String[]{};
+        System.out.println(str.length);
+        String[] str1 = {"str1", "str1", "str3", null};
+        System.out.println(str1.length);
+        String[] str2 = new String[3];
+    }
 }
